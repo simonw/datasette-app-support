d43520ffe226d44b20b4d8ad40d911cddbbab353
Simon Willison <swillison@gmail.com>
2021-09-02 11:37:29 -0700

    Better test for /-/open-csv-file
    Refs https://github.com/simonw/datasette-app/issues/29

--- tests/test_open_csv_file.py
+++ tests/test_open_csv_file.py
@@ -10,6 +10,6 @@ async def test_open_csv_files(tmpdir):
     open(path, "w").write("id,name\n123,Hello")
     response = await datasette.client.post("/-/open-csv-file", json={"path": path})
     assert response.status_code == 200
-    assert response.json()["ok"] is True
-    response = await datasette.client.get("/temporary/demo.json?_shape=array")
-    assert response.json() == [{"rowid": 1, "id": "123", "name": "Hello"}]
+    assert response.json() == {"ok": True, "path": "/temporary/demo", "rows": 1}
+    response2 = await datasette.client.get("/temporary/demo.json?_shape=array")
+    assert response2.json() == [{"rowid": 1, "id": "123", "name": "Hello"}]
